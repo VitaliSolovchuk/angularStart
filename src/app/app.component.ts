a36ef11849fc5654050380f824346191a1c88946
Vitali Solovchuk <v.solovchuk@softteco.com>
2022-08-04 14:33:45 +0300

    added error.service heandler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from "./models/product";
 
-import { products as data } from './data/products'
+// import { products as data } from './data/products'
+import { ProductsService } from "./services/products.service";
+import { Observable, tap } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularStart';
+export class AppComponent implements OnInit {
+    title = 'angularStart'
+    isLoading: boolean = false
 
-  products: IProduct[] = data
+    products$: Observable<IProduct[]>
+
+    constructor(private productsService: ProductsService) {
+    }
+
+    ngOnInit(): void {
+        this.isLoading = true
+        this.products$ = this.productsService.getAll().pipe(
+            tap(() => this.isLoading = false)
+        )
+        // this.productsService.getAll().subscribe((products) => {
+        //     this.products = products
+        //     console.log(products)
+        //     this.isLoading = false
+        // })
+    }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { IProduct } from "../../models/product";
+import { ProductsService } from "../../services/products.service";
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html',
 })
 
-export class ProductComponent {
+export class ProductComponent{
     @Input() product: IProduct
 
     details: boolean = false
+
 }
